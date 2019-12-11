1d31723f3d5233b29735ac4abf499ca28cc7c089
Gilberto <peter01@hotmail.es>
2019-12-11 07:19:35 -0500

    First commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const Product = require('../models/product');
 const fileHelper = require('../util/file');
-const mongoose = require('mongoose');
 const moment = require('moment');
+const AWS = require('aws-sdk');
 
 const { validationResult } = require('express-validator/check');
 
@@ -59,28 +60,62 @@ exports.postAddProduct = async (req, res, next) => {
       validationErrors: errors.array()
     });
   }
+  const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
 
-  const imageUrl = image.path;
-
-  const product = new Product({
-    title: title,
-    fecha: fecha,
-    description: description,
-    imageUrl: imageUrl,
-    userId: req.user
+  let s3bucket = new AWS.S3({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+    region: process.env.AWS_REGION
   });
-  product
-    .save()
-    .then(result => {
-      // console.log(result);
-      console.log('Created Product');
-      res.redirect('/admin/products');
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+
+  //Where you want to store your file
+  const keyName = new Date().getTime() + image.originalname;
+  var params = {
+    Bucket: 'maderito-bucket',
+    Key: keyName,
+    Body: image.buffer,
+    ContentType: image.mimetype,
+    ACL: "public-read"
+  };
+
+  s3bucket.upload(params, function (err, data) {
+    if (err) {
+      return res.status(422).render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false,
+        hasError: true,
+        isAdmin: req.user.isAdmin,
+        product: {
+          title: title,
+          fecha: fecha,
+          description: description
+        },
+        errorMessage: 'Attached file is not an image.',
+        validationErrors: []
+      });
+    }
+    const product = new Product({
+      title: title,
+      fecha: fecha,
+      description: description,
+      imageUrl: s3FileURL + keyName,
+      userId: req.user
     });
+    product
+      .save()
+      .then(result => {
+        // console.log(result);
+        console.log('Created Product');
+        res.redirect('/admin/products');
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      });
+  });
+
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -113,6 +148,12 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
+  let s3bucket = new AWS.S3({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+    region: process.env.AWS_REGION
+  });
+  const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedFecha = req.body.fecha;
@@ -149,12 +190,31 @@ exports.postEditProduct = (req, res, next) => {
       product.description = updatedDesc;
       product.fecha = updatedFecha;
       product.estado = updatedEstado;
-      console.log("eSTADO " + updatedEstado);
       if (image) {
-        fileHelper.deleteFile(product.imageUrl);
-        product.imageUrl = image.path;
+        const imagePath = product.imageUrl.slice(51);
+        s3bucket.deleteObject({
+          Bucket: 'maderito-bucket',
+          Key: imagePath
+        }, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+        const keyName = new Date().getTime() + image.originalname;
+        var params = {
+          Bucket: 'maderito-bucket',
+          Key: keyName,
+          Body: image.buffer,
+          ContentType: image.mimetype,
+          ACL: "public-read"
+        };
+        product.imageUrl = s3FileURL + keyName;
+        s3bucket.upload(params, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
       }
-
       return product.save()
         .then(() => {
           console.log('UPDATED PRODUCT!');
@@ -209,6 +269,11 @@ exports.getProducts = async (req, res, next) => {
 };
 
 exports.deleteProduct = (req, res, next) => {
+  let s3bucket = new AWS.S3({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+    region: process.env.AWS_REGION
+  });
   const prodId = req.body.productId;
   if (req.user.isAdmin) {
     Product.findById(prodId)
@@ -216,7 +281,15 @@ exports.deleteProduct = (req, res, next) => {
         if (!product) {
           return next(new Error('Product not found!'));
         }
-        fileHelper.deleteFile(product.imageUrl);
+        const keyName = product.imageUrl.slice(51);
+        s3bucket.deleteObject({
+          Bucket: 'maderito-bucket',
+          Key: keyName
+        }, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
         return Product.deleteOne({ _id: prodId });
       })
       .then(() => {
@@ -234,7 +307,15 @@ exports.deleteProduct = (req, res, next) => {
         if (!product) {
           return next(new Error('Product not found!'));
         }
-        fileHelper.deleteFile(product.imageUrl);
+        const keyName = product.imageUrl.slice(51);
+        s3bucket.deleteObject({
+          Bucket: 'maderito-bucket',
+          Key: keyName
+        }, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
         return Product.deleteOne({ _id: prodId, userId: req.user._id });
       })
       .then(() => {
