4731e0e8bb778da84d4931ab168b9ba39bf9859d
Gilberto <peter01@hotmail.es>
2019-12-12 15:02:29 -0500

    Saving changes

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
-const multer = require('multer');
 const compression = require('compression');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -24,24 +24,35 @@ const store = new MongoDBStore({
 });
 app.use(helmet());
 app.use(compression());
+app.use(cors());
 
-const csrfProtection = csrf();
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().getTime() + '-' + file.originalname);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
   }
+  next();
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+const csrfProtection = csrf();
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './images');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().getTime() + '-' + file.originalname);
+//   }
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -51,7 +62,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('imageUrl'));
+// app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('imageUrl'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
@@ -113,7 +124,7 @@ app.use((err, req, res) => {
 mongoose
   .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    app.listen(8080);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,17 @@ const { body } = require('express-validator/check')
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
+const fileFilter = (req, file, cb) => {
+   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
+};
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
 const router = express.Router();
 
 // /admin/add-product => GET
@@ -17,28 +28,28 @@ router.get('/products', isAuth, adminController.getProducts);
 router.get("/agenda", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', [
-   body('title')
+router.post('/add-product', upload.single("imageUrl"),
+   [body('title')
       .isString()
       .isLength({ min: 3 })
       .trim(),
    body('description')
       .isLength({ min: 8, max: 250 })
       .trim()
-], isAuth, adminController.postAddProduct);
+   ], isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', [
-   body('title')
+router.post('/edit-product', upload.single("imageUrl"),
+   [body('title')
       .isString()
       .isLength({ min: 3 })
       .trim(),
    body('description')
       .isLength({ min: 8, max: 250 })
       .trim()
-], isAuth, adminController.postEditProduct);
+   ], isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', isAuth, adminController.deleteProduct);
+router.post('/delete-product', upload.single("imageUrl"), isAuth, adminController.deleteProduct);
 
 module.exports = router;
